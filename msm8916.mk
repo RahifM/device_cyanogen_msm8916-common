771ec0a2f40736c2c044c3f729049b0e48a99c9b
Bruno Martins <bgcngm@gmail.com>
2018-02-07 06:11:00 +0530

    msm8916-common: Use MIUI poweroff charging animation
     * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794
    Signed-off-by: RahifM <faizel326@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -99,7 +99,8 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-service \
-    android.hardware.health@1.0
+    android.hardware.health@1.0 \
+    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
