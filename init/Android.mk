6eb910ffccb6296e1966a60a34922c9b663eab3f
RahifM <faizel326@gmail.com>
2018-02-19 21:35:45 +0530

    Revert "msm8916-common: Update init for O"
    This reverts commit ad796e04ca530b46c8900d49bcf4a9027a7ab9a3.

--- init/Android.mk
+++ init/Android.mk
@@ -5,14 +5,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
     system/core/base/include \
-    system/core/init \
-    external/selinux/libselinux/include
+    system/core/init
 LOCAL_CFLAGS := -Wall
 LOCAL_SRC_FILES := init_msm8916.cpp
 ifneq ($(TARGET_LIBINIT_MSM8916_DEFINES_FILE),)
   LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8916_DEFINES_FILE)
 endif
 LOCAL_MODULE := libinit_msm8916
-LOCAL_STATIC_LIBRARIES := libbase libselinux
 
 include $(BUILD_STATIC_LIBRARY)
