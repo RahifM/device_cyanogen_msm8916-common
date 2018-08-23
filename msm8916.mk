eb07a4a5fb390c3847086bf7f5ac11af38984257
RahifM <faizel326@gmail.com>
2018-08-23 21:14:47 +0000

    msm8916-common: Drop wifi_offload
    Signed-off-by: RahifM <faizel326@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -214,8 +214,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
-    android.hardware.wifi.offload@1.0-service
+    android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
