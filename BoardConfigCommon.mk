89fe60889db0ea54cf4ea511e4e5e68ec9777f4f
RahifM <faizel326@gmail.com>
2018-12-30 23:36:48 +0530

    msm8916-common: Fix OMX fekupp!?
     * Kanged from: https://forum.xda-developers.com/redmi-2/how-to/fix-instagram-wa-play-videos-lineage-os-t3872017
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,7 +137,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
 
