cd3684ff3ec33693fe4691d9b583db17fdb4959e
RahifM <faizel326@gmail.com>
2019-06-13 09:14:23 +0530

    Revert "temp"
    This reverts commit 3a29858a08ae7dc1641a8f214cca5c359ca4e0bd.

--- msm8916.mk
+++ msm8916.mk
@@ -54,10 +54,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
-# Charger
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/sbin/chargeonlymode:root/sbin/charger
-
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8916 \
